11cfdae95f350df818f7d7ddea852876dda066ba
zhengyu895456 <296180982@qq.com>
2019-12-06 22:40:16 +0800

    代码块

--- src/test/java/com/zeroten/flow/IfElseTest.java
+++ src/test/java/com/zeroten/flow/IfElseTest.java
@@ -2,6 +2,20 @@ package com.zeroten.flow;
 
 import org.testng.annotations.Test;
 
+public class CodeBlock{
+    @Test
+    public void testBlockScope(){
+        int n1 = 100;
+        System.out.println(n1);
+        {
+            int n2 = 200;
+            //int n1 = 200;
+            System.out.println(n1);
+        }
+        int n2 = 201;
+        System.out.println(n2);
+    }
+}
 public class IfElseTest {
 
     @Test
